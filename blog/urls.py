11293510a62d0f31a8120f2f40dcb75ffda34ded
darko <ki14.skakun@gmail.com>
2023-10-29 10:45:38 +0200

    inital project setup

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home_page, name="home"),
+    path("posts", views.posts, name="posts"),
+    path("posts/<slug:slug>", views.post_detail, name="posts-detail"),
+]
