bb0a71a657377879e59046cf2b12b9abe51eaf07
darko <ki14.skakun@gmail.com>
2023-11-01 14:11:10 +0200

    fetch data from db

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,19 @@ from django.core.validators import MinLengthValidator, EmailValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(validators=[EmailValidator()])
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
 
 
 class Post(models.Model):
@@ -20,8 +29,6 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
-
-
-
--- blog/views.py
+++ blog/views.py
@@ -1,93 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
+from .models import Post, Author, Tag
+
 # Create your views here.
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Darko",
-        "date": date(2023, 11 ,1),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothng like the views you get when hiking in the mountains! And I wasn't even happened while I was enjoying the view!",
-        "content": """
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Minus consequuntur voluptatibus beatae fugiat optio est, 
-                    vero distinctio perferendis enim saepe quisquam assumenda natus, 
-                    pariatur animi odio sit repudiandae quidem dolore?
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Minus consequuntur voluptatibus beatae fugiat optio est, 
-                    vero distinctio perferendis enim saepe quisquam assumenda natus, 
-                    pariatur animi odio sit repudiandae quidem dolore?
-
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Minus consequuntur voluptatibus beatae fugiat optio est, 
-                    vero distinctio perferendis enim saepe quisquam assumenda natus, 
-                    pariatur animi odio sit repudiandae quidem dolore?
-                    """
-
-
-    },
-     {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Darko",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Darko",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-
-]
-
-def get_date(post):
-    return post.get("date")
 
 def home_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     context = {"posts": latest_posts}
     return render(request, "blog/index.html", context)
 
+
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     context = {"posts": all_posts}
     return render(request, "blog/all-posts.html", context)
 
+
 def post_detail(request, slug):
-    post = next(post for post in all_posts if post["slug"] == slug)
-    context = {"post": post}
+    post = get_object_or_404(Post, slug=slug)
+    context = {"post": post, "tags": post.tags.all()}
     return render(request, "blog/post-detail.html", context)
