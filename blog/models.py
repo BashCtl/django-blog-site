f3854f89e0df549b6aa26a65225646dc53ba46a1
darko <ki14.skakun@gmail.com>
2023-11-04 11:09:40 +0200

    added comment db model

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,10 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    useername= models.CharField(max_length=120)
+    user_eamil = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
